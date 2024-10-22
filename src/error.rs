01bce8d8b17e86d9f5591039192a86d6d540f8aa
Olivier Poppon <olivier.poppon@tempo-one.com>
2024-10-22 11:11:45 +0200

    impl Display sur Error

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,27 @@
+use std::fmt::{Display, Formatter};
+
 use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Clone, Debug, Serialize)]
 pub enum Error {
-   Parsing(String),
+    Parsing(String),
 
-   MissingParam(String),
+    MissingParam(String),
 
-   ApiConnexion(String),
-   ApiResult(String),
+    ApiConnexion(String),
+    ApiResult(String),
 }
 
 impl From<serde_json::Error> for Error {
-   fn from(e: serde_json::Error) -> Self {
-       Error::Parsing(e.to_string())
-   }
+    fn from(e: serde_json::Error) -> Self {
+        Error::Parsing(e.to_string())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
 }
