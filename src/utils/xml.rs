b8d514cc7e232757b4b43abed3672c614e00ba31
Olivier Poppon <olivier.poppon@tempo-one.com>
2025-09-11 16:57:12 +0200

    crate upgrade AND utilisation if let Some && let Some

--- src/utils/xml.rs
+++ src/utils/xml.rs
@@ -2,12 +2,12 @@ pub fn extract_xml_tag<'a>(tag: &str, value: &'a str) -> Option<&'a str> {
     let begin = value.find(&format!("<{tag}"));
     let end = value.find(&format!("</{tag}>"));
 
-    if begin.is_none() || end.is_none() {
-        None
-    } else {
-        let end = end.unwrap() + tag.len() + "</>".len();
-        let part = &value[begin.unwrap()..end];
+    if let Some(begin) = begin && let Some(end) = end {
+        let end = end + tag.len() + "</>".len();
+        let part = &value[begin..end];
 
         Some(part)
+    } else {
+        None
     }
 }
